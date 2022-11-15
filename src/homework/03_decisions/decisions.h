04458b7c61576bc290d4f85781e89d933c7d9e03
alat414 <111584946+alat414@users.noreply.github.com>
2022-11-15 15:15:09 -0600

    Update decisions.h

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,8 +1,19 @@
 //write include required statements
+#include<string>
+
+using std::string; 
+
+//  write function prototypes for get_letter_grade_using_if and get_letter_grade_using_switch. (see Function specifications table)
+
+
+string menu (int grade);
+
 
 
 //Write a function(s) prototype  here
 
+string get_letter_grade_using_if(int grade);
+string get_letter_grade_using_switch(int grade);
 
 
 
