6e98519e155ecd9269284bccbf5a43a7f4c86d36
alat414 <gustavo.alatriste@g.austincc.edu>
2023-07-19 16:20:38 -0500

    changes done

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,4 +1,11 @@
 ﻿//write include statements
+#include "data_types.h"
+
+#include <iostream>
+
+using namespace std;
+
+
 
 
 //write namespace using statement for cout
@@ -7,8 +14,20 @@
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
+
+int num1 = 4, result;
+
 int main()
 {
+	int num;
+
+	std::cout << "Enter the value for int variable. ";
+	cin >> num;
+
+	std::cout << "the first int variable is: " << num << endl;
+
+	result = multiply_numbers(num1);
+	cout << num1 << result;
 	
 
 	return 0;
